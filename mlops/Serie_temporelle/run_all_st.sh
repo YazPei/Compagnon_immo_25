fbca919b51bc05d3b7f5ec43f4e2bdf801d63deb
Yasmine Peiffer <peifferyasmine@gmail.com>
2025-07-02 13:24:30 +0200

    Révision de l'arborescence

---
+++ mlops/Serie_temporelle/run_all_st.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+echo "🔁 Lancement du pipeline des Séries Temporelles avec DVC"
+dvc repro evaluate
+
+echo "📈 Visualisation du graphe"
+dvc dag
+
+#echo "⚙️ Étape 1 : Split temporel"
+#docker compose run split
+
+#echo "🌀 Étape 2 : Décomposition saisonnière"
+#docker compose run decompose
+
+#echo "📈 Étape 3 : Entraînement SARIMAX"
+#docker compose run train_sarimax
+
+#echo "📊 Étape 4 : Évaluation"
+#docker compose run evaluate
+
+echo "✅ Pipeline Serie Temporelle terminée avec succès."
+
+

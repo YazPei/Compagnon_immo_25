ffc3fa62f01ca1a00994a7a4c8cd01dc2f4c2cdc
faithpedro <ketsia.pedro@hetic.net>
2025-09-26 04:30:00 +0200

    gestion du hashage des mots de passe en prod

--- app/api/security/auth.py
+++ app/api/security/auth.py
@@ -70,11 +70,11 @@ class AuthManager:
         self, plain_password: str, hashed_password: str
     ) -> bool:
         """Vérifie si un mot de passe correspond à son hash."""
-        return pwd_context.verify(plain_password, hashed_password)
+        return pwd_context.verify(plain_password[:72], hashed_password)
 
     def get_password_hash(self, password: str) -> str:
         """Hash un mot de passe."""
-        return pwd_context.hash(password)
+        return pwd_context.hash(password[:72])
 
     def create_access_token(
         self, data: Dict[str, Any], expires_delta: Optional[timedelta] = None
